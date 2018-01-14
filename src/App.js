fc705744c4f0805cfeeccad915fda4ba78882ee0
viniosouza <vinicios2508@gmail.com>
2018-01-14 04:02:07 -0200

    App with some fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+import Home from "./pages/Home";
+import CompanyProfile from "./components/CompanyProfile";
+import "antd/dist/antd.css";
+
+const componseEnhancers = composeWithDevTools({});
+
+const store = createStore(
+  reducers,
+  /* preloadedState, */ componseEnhancers(applyMiddleware(thunk))
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Route path="/" exact component={Home} />
+          <Route path="/job/:jobId" component={CompanyProfile} />
+        </div>
+      </Router>
+      // </Provider>
     );
   }
 }
